09e593dd9da6b295d85da629696652836fbc6998
Javier Martinez <javiermarsal11@gmail.com>
2025-03-14 15:07:36 +0100

    [chs] initial commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import './global.css';
+
+export default function App() {
+  return (
+    <>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-3xl font-bold">Hello</Text>
+      </View>
+    </>
+  );
+}
